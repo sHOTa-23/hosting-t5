0a4758d1d9cf876a3772e5c703307b8a91d87a74
sHOTa-23 <selka18@freeuni.edu.ge>
2025-02-15 16:10:43 +0100

    changed default prompt

--- batch_client.py
+++ batch_client.py
@@ -6,6 +6,6 @@ def generate_text(prompt: str):
     return response.json()
 
 if __name__ == "__main__":
-    prompts = ["Translate English to French: Hello!", "Summarize: The quick brown fox jumps over the lazy dog.", "Summarize: The quick brown fox jumps over the lazy dog.", "Summarize: The quick brown fox dsdasdas over the lazy dog.", "Summarize: dasdasd quick brown fox czxc over the lazy dog."] + [f"What is {i} times 2" for i in range(500)]
+    prompts = ["Translate English to French: Hello?", "Summarize: The quick brown fox jumps over the lazy dog.", "Summarize: The quick brown fox jumps over the lazy dog.", "Summarize: The quick brown fox dsdasdas over the lazy dog.", "Summarize: dasdasd quick brown fox czxc over the lazy dog."] + [f"What is {i} times 2" for i in range(500)]
     result = generate_text(prompts)
     print(result)
